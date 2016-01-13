d2e84b3de011b704867188f97efe0c64db29b28b
(cdep)illabout <cdep.illabout@gmail.com>
2016-01-13 14:21:33 +0900

    Added lifted alert function.

--- src/Browser/Alert.purs
+++ src/Browser/Alert.purs
@@ -1,10 +1,14 @@
 
 module Browser.Alert where
 
-import Prelude (Unit)
+import Prelude (Unit, (<<<))
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
 
 foreign import data ALERT :: !
 
 foreign import alert :: forall eff . String -> Eff (alert :: ALERT | eff) Unit
+
+alert' :: forall eff m . (MonadEff (alert :: ALERT | eff) m) => String -> m Unit
+alert' = liftEff <<< alert
